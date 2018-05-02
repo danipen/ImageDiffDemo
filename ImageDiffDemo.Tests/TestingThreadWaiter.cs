ede438c77bb11e448d013e2ea83b68ebdc79ebf7
Daniel <dpenalba@codicesoftware.com>
2018-05-02 03:14:01 -0700

    Added missing files

--- ImageDiffDemo.Tests/TestingThreadWaiter.cs
+++ ImageDiffDemo.Tests/TestingThreadWaiter.cs
@@ -1,10 +1,61 @@
 ﻿using System;
+using ImageDiffDemo.Common;
+
 namespace ImageDiffDemo.Tests
 {
-    public class TestingThreadWaiter
+    public class TestingThreadWaiterBuilder : IThreadWaiterBuilder
     {
-        public TestingThreadWaiter()
+        IThreadWaiter IThreadWaiterBuilder.GetWaiter()
+        {
+            return new TestingThreadWaiter();
+        }
+
+        IThreadWaiter IThreadWaiterBuilder.GetWaiter(int timerInterval)
+        {
+            return new TestingThreadWaiter();
+        }
+
+        IThreadWaiter IThreadWaiterBuilder.GetModalWaiter()
+        {
+            return new TestingThreadWaiter();
+        }
+    }
+
+    public class TestingThreadWaiter : IThreadWaiter
+    {
+        public Exception Exception { get { return mException; } }
+
+        public void Execute(
+            PlasticThread.Operation threadOperationDelegate,
+            PlasticThread.Operation afterOperationDelegate)
+        {
+            Execute(threadOperationDelegate, afterOperationDelegate, null);
+        }
+
+        public void Execute(
+            PlasticThread.Operation threadOperationDelegate,
+            PlasticThread.Operation afterOperationDelegate,
+            PlasticThread.Operation timerTickDelegate)
+        {
+            try
+            {
+                threadOperationDelegate();
+            }
+            catch (Exception ex)
+            {
+                mException = ex;
+            }
+
+            if (timerTickDelegate != null)
+                timerTickDelegate();
+
+            afterOperationDelegate();
+        }
+
+        public void Cancel()
         {
         }
+
+        Exception mException;
     }
 }
